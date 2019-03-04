633fa8b0d7ecff845a5c7db977d8a1268a7b2811
Luis M. Barquillo <lmbarquillo@gmail.com>
2019-03-04 19:03:17 +0100

    Cambiada petición del team y ambas a gente de España

--- js/scriptteam.js
+++ js/scriptteam.js
@@ -1,4 +1,4 @@
-const API = "https://randomuser.me/api/";
+const API = "https://randomuser.me/api/?results=4&nat=ES";
 var gente = [];
 
 $(document).ready(() => {
@@ -8,24 +8,23 @@ $(document).ready(() => {
 
 function cargarGente() {
     // Cargamos 4 interfectos, pero en lugar de usar fetch como en el home, aquí usamos ajax
-    for(let i=0; i<4; i++) {
-        $.ajax({
-            url: API,
-            type: 'GET',
-            success: function(data) {
-                gente.push(data);
-                mostrarGente();
-            },
-            error: function(error) {
-                console.log(error);
-            }
-        });	
-    }    
+    $.ajax({
+        url: API,
+        type: 'GET',
+        success: function(data) {
+            gente = data.results;
+            console.log(gente);
+            mostrarGente();
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });	   
 }
 
 function mostrarGente() {
     for(let i=1; i<=gente.length; i++) {
-		let data = gente[i-1].results[0];
+		let data = gente[i-1];
 
         $("#expert-img"+i).attr("src", data.picture.large);
         $("#expert-name"+i).html(data.name.first.toUpperCase() + ' ' + data.name.last.toUpperCase());
